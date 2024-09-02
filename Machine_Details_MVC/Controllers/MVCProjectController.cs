f3af2d59275ee0b4cedd0232b85138a79fe68c97
Vaibhav Waghamode <vaibhavwa@cybage.com>
2024-09-02 15:01:11 +0530

    Sep First commit

---
+++ Machine_Details_MVC/Controllers/MVCProjectController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http; 
+namespace MVC_Project.Controllers
+{
+    public class MVCProjectController : Controller
+    {
+
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+
+        public MVCProjectController(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var URL = _config.GetValue<string>("API_URL");
+            var response = await _httpClient.GetAsync(URL);
+            var resultString = await response.Content.ReadAsStringAsync();
+            dynamic result = JsonConvert.DeserializeObject(resultString);
+            return View(result);
+        }
+    }
+}
